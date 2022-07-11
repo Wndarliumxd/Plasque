8566a42fe9927f0d3d1615b5cfce6bc49952f19e
DESKTOP-R8L3GPF\josea <joseangelrivas81@gmail.com>
2022-07-11 16:46:08 -0600

    Cree opciones en barra de ventana

--- Plasque/Form1.Designer.cs
+++ Plasque/Form1.Designer.cs
@@ -28,34 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.panelCabeza = new System.Windows.Forms.Panel();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.panelContenedor = new System.Windows.Forms.Panel();
+            this.botonRestaurar = new System.Windows.Forms.Button();
+            this.botonMin = new System.Windows.Forms.Button();
+            this.botonMax = new System.Windows.Forms.Button();
+            this.botonSalida = new System.Windows.Forms.Button();
+            this.panelCabeza.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // panelCabeza
             // 
-            this.button1.Location = new System.Drawing.Point(100, 176);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.panelCabeza.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panelCabeza.Controls.Add(this.botonRestaurar);
+            this.panelCabeza.Controls.Add(this.botonMin);
+            this.panelCabeza.Controls.Add(this.botonMax);
+            this.panelCabeza.Controls.Add(this.botonSalida);
+            this.panelCabeza.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelCabeza.Location = new System.Drawing.Point(0, 0);
+            this.panelCabeza.Name = "panelCabeza";
+            this.panelCabeza.Size = new System.Drawing.Size(745, 40);
+            this.panelCabeza.TabIndex = 0;
+            this.panelCabeza.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelCabeza_MouseMove);
+            // 
+            // panelMenu
+            // 
+            this.panelMenu.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.panelMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelMenu.Location = new System.Drawing.Point(0, 40);
+            this.panelMenu.Name = "panelMenu";
+            this.panelMenu.Size = new System.Drawing.Size(200, 427);
+            this.panelMenu.TabIndex = 1;
+            // 
+            // panelContenedor
+            // 
+            this.panelContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContenedor.Location = new System.Drawing.Point(200, 40);
+            this.panelContenedor.Name = "panelContenedor";
+            this.panelContenedor.Size = new System.Drawing.Size(545, 427);
+            this.panelContenedor.TabIndex = 2;
+            this.panelContenedor.Visible = false;
+            // 
+            // botonRestaurar
+            // 
+            this.botonRestaurar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.botonRestaurar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.botonRestaurar.FlatAppearance.BorderSize = 0;
+            this.botonRestaurar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.botonRestaurar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.botonRestaurar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonRestaurar.Image = global::Plasque.Properties.Resources.Icono_Restaurar;
+            this.botonRestaurar.Location = new System.Drawing.Point(659, 0);
+            this.botonRestaurar.Name = "botonRestaurar";
+            this.botonRestaurar.Size = new System.Drawing.Size(40, 40);
+            this.botonRestaurar.TabIndex = 3;
+            this.botonRestaurar.UseVisualStyleBackColor = true;
+            this.botonRestaurar.Click += new System.EventHandler(this.botonRestaurar_Click);
+            // 
+            // botonMin
+            // 
+            this.botonMin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.botonMin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.botonMin.FlatAppearance.BorderSize = 0;
+            this.botonMin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.botonMin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.botonMin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonMin.Image = global::Plasque.Properties.Resources.Icono_Minimizar;
+            this.botonMin.Location = new System.Drawing.Point(613, 0);
+            this.botonMin.Name = "botonMin";
+            this.botonMin.Size = new System.Drawing.Size(40, 40);
+            this.botonMin.TabIndex = 2;
+            this.botonMin.UseVisualStyleBackColor = true;
+            this.botonMin.Click += new System.EventHandler(this.botonMin_Click);
+            // 
+            // botonMax
+            // 
+            this.botonMax.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.botonMax.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.botonMax.FlatAppearance.BorderSize = 0;
+            this.botonMax.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.botonMax.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.botonMax.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonMax.Image = global::Plasque.Properties.Resources.Icono_Maximizar;
+            this.botonMax.Location = new System.Drawing.Point(659, 0);
+            this.botonMax.Name = "botonMax";
+            this.botonMax.Size = new System.Drawing.Size(40, 40);
+            this.botonMax.TabIndex = 1;
+            this.botonMax.UseVisualStyleBackColor = true;
+            this.botonMax.Click += new System.EventHandler(this.botonMax_Click);
+            // 
+            // botonSalida
+            // 
+            this.botonSalida.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.botonSalida.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.botonSalida.FlatAppearance.BorderSize = 0;
+            this.botonSalida.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.botonSalida.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.botonSalida.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.botonSalida.Image = global::Plasque.Properties.Resources.Icono_cerrar_FN;
+            this.botonSalida.Location = new System.Drawing.Point(705, 0);
+            this.botonSalida.Name = "botonSalida";
+            this.botonSalida.Size = new System.Drawing.Size(40, 40);
+            this.botonSalida.TabIndex = 0;
+            this.botonSalida.UseVisualStyleBackColor = true;
+            this.botonSalida.Click += new System.EventHandler(this.botonSalida_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(745, 467);
+            this.Controls.Add(this.panelContenedor);
+            this.Controls.Add(this.panelMenu);
+            this.Controls.Add(this.panelCabeza);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panelCabeza.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelCabeza;
+        private System.Windows.Forms.Button botonSalida;
+        private System.Windows.Forms.Panel panelMenu;
+        private System.Windows.Forms.Panel panelContenedor;
+        private System.Windows.Forms.Button botonMax;
+        private System.Windows.Forms.Button botonMin;
+        private System.Windows.Forms.Button botonRestaurar;
+
     }
 }
 
--- Plasque/Form1.cs
+++ Plasque/Form1.cs
@@ -7,19 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Plasque
 {
+
     public partial class Form1 : Form
     {
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
         public Form1()
         {
             InitializeComponent();
+            botonRestaurar.Visible = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //secl
+        }
+
+        private void botonSalida_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void botonMax_Click(object sender, EventArgs e)
+        {
+            botonMax.Visible = false;
+            botonRestaurar.Visible = true;
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void botonRestaurar_Click(object sender, EventArgs e)
+        {
+            botonRestaurar.Visible = false;
+            botonMax.Visible = true;
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void botonMin_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void panelCabeza_MouseMove(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
         }
     }
 }
